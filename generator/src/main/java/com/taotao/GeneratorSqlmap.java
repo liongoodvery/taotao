04626b014f744652d059956b6e87b0d299a134e9
lvqingfeng <lion_good@163.com>
2018-07-08 01:17:29 +0800

    test

--- generator/src/main/java/com/taotao/GeneratorSqlmap.java
+++ generator/src/main/java/com/taotao/GeneratorSqlmap.java
@@ -6,33 +6,35 @@ import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.internal.DefaultShellCallback;
 
 import java.io.File;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GeneratorSqlmap {
 
-	public void generator() throws Exception{
+    public void generator() throws Exception {
 
-		List<String> warnings = new ArrayList<String>();
-		boolean overwrite = true;
-		//指定 逆向工程配置文件
-		File configFile = new File("generatorConfig.xml"); 
-		ConfigurationParser cp = new ConfigurationParser(warnings);
-		Configuration config = cp.parseConfiguration(configFile);
-		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
-		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
-				callback, warnings);
-		myBatisGenerator.generate(null);
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        InputStream resource = getClass().getClassLoader().getResourceAsStream("generatorConfig.xml");
+        //指定 逆向工程配置文件
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(resource);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+                callback, warnings);
+        myBatisGenerator.generate(null);
 
-	} 
-	public static void main(String[] args) throws Exception {
-		try {
-			GeneratorSqlmap generatorSqlmap = new GeneratorSqlmap();
-			generatorSqlmap.generator();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-	}
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            GeneratorSqlmap generatorSqlmap = new GeneratorSqlmap();
+            generatorSqlmap.generator();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
